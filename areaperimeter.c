dd65ec7288be2af6085aca133303a2feb1c32a34
Adityapartap1 <76278413+Adityapartap1@users.noreply.github.com>
2021-03-17 21:45:15 +0530

    Areaperimeter

---
+++ areaperimeter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
+
+	raduisOfCircle=7;
+	sideOfSquare=4;
+	legthOfRectangle=3;
+	breadthOfRectangle=5;
+	
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
+	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
+	return 0;
+}
